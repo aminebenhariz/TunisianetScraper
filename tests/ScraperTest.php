5c8a5339b368732395814c90e96cb577134868da
Amine Ben Hariz <aminebenhariz@gmail.com>
2016-01-21 15:50:30 +0100

    add getDescription

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -19,6 +19,7 @@ class ScraperTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(true, $product->isAvailable());
         $this->assertSame('HUA722020ALA331', $product->getReference());
         $this->assertSame('Disque Dur Interne 3.5" Hitachi UltraStar 2 To', $product->getTitle());
+        $this->assertStringStartsWith('Disque Dur Interne 3.5" Hitachi UltraStar - Inter', $product->getDescription());
         $this->assertSame(131.000, $product->getPrice());
     }
 }
