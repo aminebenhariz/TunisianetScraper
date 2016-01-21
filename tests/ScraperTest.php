1cc39214ed614bc2f3594d772048c8744b54211b
Amine Ben Hariz <aminebenhariz@gmail.com>
2016-01-21 15:09:46 +0100

    add online test

---
+++ tests/ScraperTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AmineBenHariz\TunisianetScraper\Tests;
+
+use AmineBenHariz\TunisianetScraper\Scraper;
+
+/**
+ * Class ScraperTest
+ * @package AmineBenHariz\TunisianetScraper\Tests
+ */
+class ScraperTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetProductById()
+    {
+        $scraper = new Scraper();
+        $product = $scraper->getProductById(15800);
+
+        $this->assertSame(true, $product->isAvailable());
+        $this->assertSame('HUA722020ALA331', $product->getReference());
+        $this->assertSame('Disque Dur Interne 3.5" Hitachi UltraStar 2 To', $product->getTitle());
+        $this->assertSame(131.000, $product->getPrice());
+    }
+}
