3760304f93ce8c895ab06035faefb8124171e23a
Amine Ben Hariz <aminebenhariz@gmail.com>
2016-08-05 14:39:40 +0100

    fix test values

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -14,28 +14,38 @@ class ScraperTest extends \PHPUnit_Framework_TestCase
     public function testGetProductById()
     {
         $scraper = new Scraper();
-        $product = $scraper->getProductById(15800);
+        $product = $scraper->getProductById(17243);
 
-        $this->assertSame(true, $product->isAvailable());
-        $this->assertSame('HUA722020ALA331', $product->getReference());
-        $this->assertSame('Disque Dur Interne 3.5" Hitachi UltraStar 2 To', $product->getTitle());
-        $this->assertStringStartsWith('Disque Dur Interne 3.5" Hitachi UltraStar - Inter', $product->getDescription());
-        $this->assertSame(131.000, $product->getPrice());
+        $this->assertSame(true, $product->isAvailable(), 'product not available');
+        $this->assertSame('CARBIDE-400C', $product->getReference(), 'invalid reference');
+        $this->assertSame('Boitier Corsair Carbide 400C', $product->getTitle(), 'invalid title');
+        $this->assertStringStartsWith(
+            'Boîtier Moyen Tour en acier avec fenêtre',
+            $product->getDescription(),
+            'invalid description'
+        );
+        $this->assertSame(265.000, $product->getPrice(), 'invalid price');
         $this->assertSame(
-            'http://www.tunisianet.com.tn/disques-durs-internes/15800-disque-dur-interne-35-hitachi-ultrastar-2-to.html',
-            $product->getUrl()
+            'http://www.tunisianet.com.tn/boitier-pour-pc-de-bureau/17243-boitier-corsair-carbide-400c.html',
+            $product->getUrl(),
+            'invalid url'
         );
     }
 
     public function testGetCategoryById()
     {
         $scraper = new Scraper();
-        $category = $scraper->getCategoryById(458);
+        $category = $scraper->getCategoryById(435);
 
-        $this->assertSame('http://www.tunisianet.com.tn/458-disques-durs-internes', $category->getUrl());
-        $this->assertSame('Disques Durs Internes Neufs', $category->getTitle());
-        $this->assertSame(458, $category->getId());
-        $this->assertCount(15, $category->getProducts());
+        $this->assertSame(
+            'http://www.tunisianet.com.tn/435-carte-raid-pour-serveur',
+            $category->getUrl(),
+            'invalid url'
+        );
+
+        $this->assertSame('Carte Raid pour Serveur', $category->getTitle(), 'invalid title');
+        $this->assertSame(435, $category->getId(), 'invalid id');
+        $this->assertCount(2, $category->getProducts(), 'invalid product count');
 
         $product = $category->getProducts()[0];
 
